228d8762d0000a5a5e044d1ac71828100ce8df30
Michele Sette <msette@codearchitects.com>
2025-01-20 16:58:08 +0100

    Aggiunto il visualizza task, visualizza progetti per team e modifica task

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const Progetto_1 = require("./Progetto");
 const Task_1 = require("./Task");
 //#region Aggiungi Prodotto
 let progettoUno = new Progetto_1.Progetto(0, "Apple", "Mighele Colucci", new Date("2025-01-09"), new Date("2025-01-15"), 1);
-let progettoDue = new Progetto_1.Progetto(1, "Samsung", "Mighele Colucci", new Date("2025-01-14"), new Date("2025-01-15"), 1);
+let progettoDue = new Progetto_1.Progetto(1, "Samsung", "Davide Sette", new Date("2025-01-14"), new Date("2025-01-15"), 1);
 let progettoTre = new Progetto_1.Progetto(0, "Huawei", "Mighele Colucci", new Date("2025-01-13"), new Date("2025-01-15"), 1);
-let progettoQuattro = new Progetto_1.Progetto(3, "Nokia", "Mighele Colucci", new Date("2025-01-11"), new Date("2025-01-15"), 1);
+let progettoQuattro = new Progetto_1.Progetto(3, "Nokia", "Davide Sette", new Date("2025-01-11"), new Date("2025-01-15"), 1);
 let progetti = [
     progettoUno,
     progettoDue,
@@ -15,12 +15,19 @@ let progetti = [
 ];
 console.table(progetti);
 let idsVisti = new Set();
+let teamVisti = new Set();
 progetti.forEach((progetto) => {
     if (idsVisti.has(progetto.id)) {
-        console.log(`ID duplicato trovato: ${progetto.id} - Progetto: ${progetto.nome}`);
+        console.log(`ID progetto duplicato trovato: ${progetto.id} - Progetto: ${progetto.nome}`);
     }
     else {
         idsVisti.add(progetto.id);
+    }
+});
+//visualizza progetti per team
+progetti.forEach((progetto) => {
+    if (progetto.team == "Mighele Colucci") {
+        console.table(progetto);
     }
 });
 progetti.splice(3);
@@ -29,8 +36,8 @@ console.table(progetti);
 //#region Aggiungi Task
 let taskUno = new Task_1.Task(1, "crazione di un nuovo iphone", Task_1.Stato.InProgress, Task_1.Priorita.Alta, "Domenigo");
 let taskDue = new Task_1.Task(2, "crazione di un nuovo Samsung", Task_1.Stato.ToDo, Task_1.Priorita.Bassa, "Domenigo");
-let taskTre = new Task_1.Task(1, "crazione di un nuovo Huawei", Task_1.Stato.Done, Task_1.Priorita.Media, "Domenigo");
-let taskQuattro = new Task_1.Task(3, "crazione di un nuovo Nokia", Task_1.Stato.InProgress, Task_1.Priorita.Alta, "Domenigo");
+let taskTre = new Task_1.Task(3, "crazione di un nuovo Huawei", Task_1.Stato.Done, Task_1.Priorita.Media, "Domenigo");
+let taskQuattro = new Task_1.Task(4, "crazione di un nuovo Nokia", Task_1.Stato.InProgress, Task_1.Priorita.Alta, "Domenigo");
 let task = [
     taskUno,
     taskDue,
@@ -38,4 +45,22 @@ let task = [
     taskQuattro
 ];
 console.table(task);
+let idControllati = new Set();
+//controllo id task duplicati
+task.forEach((task) => {
+    if (idControllati.has(task.id)) {
+        console.log(`ID task duplicato trovato: ${task.id} - Task: ${task.descrizione}`);
+    }
+    else {
+        idControllati.add(task.id);
+    }
+});
+//modifica stato task
+task.forEach((task) => {
+    if (task.id == 1) {
+        console.log(`ID task è: ${task.id} e il suo stato è: ${task.stato}`);
+        task.stato = Task_1.Stato.Done;
+        console.log(`ID task è: ${task.id} e il suo nuovo stato è: ${task.stato}`);
+    }
+});
 //#endregion
--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@ import { Priorita, Task, Stato } from "./Task";
 
 //#region Aggiungi Prodotto
 let progettoUno = new Progetto(0, "Apple", "Mighele Colucci", new Date("2025-01-09"), new Date("2025-01-15"), 1 )
-let progettoDue = new Progetto(1, "Samsung", "Mighele Colucci", new Date("2025-01-14"), new Date("2025-01-15"), 1 )
+let progettoDue = new Progetto(1, "Samsung", "Davide Sette", new Date("2025-01-14"), new Date("2025-01-15"), 1 )
 let progettoTre = new Progetto(0, "Huawei", "Mighele Colucci", new Date("2025-01-13"), new Date("2025-01-15"), 1 )
-let progettoQuattro = new Progetto(3, "Nokia", "Mighele Colucci", new Date("2025-01-11"), new Date("2025-01-15"), 1 )
+let progettoQuattro = new Progetto(3, "Nokia", "Davide Sette", new Date("2025-01-11"), new Date("2025-01-15"), 1 )
 
 let progetti: Progetto[] = [
     progettoUno,
@@ -17,15 +17,22 @@ let progetti: Progetto[] = [
 console.table(progetti);
 
 let idsVisti = new Set<number>();
+let teamVisti = new Set<string>();
 
 progetti.forEach((progetto) => {
     if (idsVisti.has(progetto.id)) {
-        console.log(`ID duplicato trovato: ${progetto.id} - Progetto: ${progetto.nome}`);
+        console.log(`ID progetto duplicato trovato: ${progetto.id} - Progetto: ${progetto.nome}`);
     } else {
         idsVisti.add(progetto.id);
     }
 });
 
+//visualizza progetti per team
+progetti.forEach((progetto) => {
+    if (progetto.team == "Mighele Colucci") {
+        console.table(progetto); 
+    }
+})
 
 progetti.splice(3);
 console.table(progetti);
@@ -34,8 +41,8 @@ console.table(progetti);
 //#region Aggiungi Task
 let taskUno = new Task(1, "crazione di un nuovo iphone", Stato.InProgress, Priorita.Alta, "Domenigo");
 let taskDue = new Task(2, "crazione di un nuovo Samsung", Stato.ToDo, Priorita.Bassa, "Domenigo")
-let taskTre = new Task(1, "crazione di un nuovo Huawei", Stato.Done, Priorita.Media, "Domenigo")
-let taskQuattro = new Task(3, "crazione di un nuovo Nokia", Stato.InProgress, Priorita.Alta, "Domenigo")
+let taskTre = new Task(3, "crazione di un nuovo Huawei", Stato.Done, Priorita.Media, "Domenigo")
+let taskQuattro = new Task(4, "crazione di un nuovo Nokia", Stato.InProgress, Priorita.Alta, "Domenigo")
 
 let task: Task[] = [
     taskUno,
@@ -44,4 +51,24 @@ let task: Task[] = [
     taskQuattro
 ] 
 console.table(task)
+
+let idControllati = new Set<number>();
+//controllo id task duplicati
+task.forEach((task) => {
+    if (idControllati.has(task.id)) {
+        console.log(`ID task duplicato trovato: ${task.id} - Task: ${task.descrizione}`);
+    } else {
+        idControllati.add(task.id);
+    }
+});
+
+//modifica stato task
+task.forEach((task) => {
+    if (task.id == 1) {
+        console.log(`ID task è: ${task.id} e il suo stato è: ${task.stato}`);
+        task.stato = Stato.Done
+        console.log(`ID task è: ${task.id} e il suo nuovo stato è: ${task.stato}`); 
+    }
+})
+
 //#endregion
